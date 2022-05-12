843f30b6694f8c4994245d3aedb32460da8eac2d
Franco Rod <francorodios@gmail.com>
2022-05-12 13:31:09 -0400

    Initial Commit

---
+++ textLink/textLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  textLinkApp.swift
+//  textLink
+//
+//  Created by Franco Rodrigues on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct textLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
